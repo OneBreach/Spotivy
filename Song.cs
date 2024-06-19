294ec208561ebea6168855e81f646785518c4d75
jorranMLGN <jorranhoukes1@gmail.com>
2024-06-19 10:35:47 +0200

    +Added client / user

---
+++ Song.cs
@@ -0,0 +1,18 @@
+namespace Spotivy;
+
+public class Song(string title, string[] artists, string genre, int duration)
+{
+    public string Title { get; set; } = title;
+    public string[] Artists { get; set; } = artists;
+    public string Genre { get; set; } = genre;
+    public int Duration { get; set; } = duration;
+
+
+
+    public Song GetSong()
+    {
+        return this;
+    }
+
+
+}
---
+++ User.cs
@@ -0,0 +1,40 @@
+namespace Spotivy;
+
+public class User(string name, List<Playlist> playlists, List<User> friends)
+{
+    public string Name { get; set; } = name;
+    private List<Playlist> Playlists { get; set; } = playlists;
+    private List<User> Friends { get; set; } = friends;
+
+    public void AddFriend(User friend)
+    {
+        Friends.Add(friend);
+    }
+
+    public void RemoveFriend(User friend)
+    {
+        Friends.Remove(friend);
+    }
+
+    public void AddPlaylist(Playlist playlist)
+    {
+        Playlists.Add(playlist);
+    }
+
+    public void RemovePlaylist(Playlist playlist)
+    {
+        Playlists.Remove(playlist);
+    }
+
+    public void AddSongToPlaylist(Song song, Playlist playlist)
+    {
+        playlist.AddSong(song);
+    }
+
+    public void RemoveSongFromPlaylist(Song song, Playlist playlist)
+    {
+        playlist.RemoveSong(song);
+    }
+
+
+}

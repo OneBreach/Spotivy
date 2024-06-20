f2dbc7c8c34b6640ac472c82d83fb10b5089a41e
MilanMaPangPang <120579337+MilanMaPangPang@users.noreply.github.com>
2024-06-20 12:00:58 +0200

    prop artist album

--- Album.cs
+++ Album.cs
@@ -1,17 +1,17 @@
 namespace Spotivy;
 
-public class Album(string title, List<Song> songs)
+public class Album(string title, List<Song> songs, List<Artist> artists)
 {
     public string Title { get; set; } = title;
     public List<Song> Songs { get; set; } = songs;
-    public List<Artist> Artists { get; set; }
+    public List<Artist> Artists { get; set; } = artists;
 
-    public void Play()
-    {
-        Console.WriteLine($"Playing album '{Title}':");
-        foreach (var song in Songs)
+        public void Play()
         {
-            song.Play();
-        }
+            Console.WriteLine($"Playing album '{Title}':");
+            foreach (var song in Songs)
+            {
+                song.Play();
+            }
     }
 }
